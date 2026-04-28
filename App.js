3ed6f43f845ddb0e0e26ccf09280737b55d9d4fd
mantishasinghal8 <mantishasinghal8@gmail.com>
2026-04-29 00:26:10 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,76 @@
+//~ Single Structure
+const heading = React.createElement(
+    "h1",//^ tag name
+    { id: "heading" }, //^ object that takes the attributes of the element  
+    "Hello World from React!"// ^ Data to be shown
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+
+/* Normal Structure
+*<div id="parent">
+* <div id="child">
+*    <h1>
+*         I'm an h1 tag
+*      </h1>
+*   </div>
+*</div>
+*
+*/
+
+
+//~ Nested Structure
+// const parent = React.createElement(
+//     "div",
+//     { id: "parent" },
+//     React.createElement(
+//         "div",
+//         { id: "child" },
+//         React.createElement(
+//             "h1",
+//             {},
+//             "I'm an h1 Tag"
+//         )
+//     )
+// )
+
+// root.render(parent)
+
+/* Siblings Structure
+We will Convert create element to an array 
+*<div id="parent">
+*   <div id="child">
+*       <h1>
+*         I'm an h1 tag
+*       </h1>
+*       <h2>
+*         I'm an h2 tag
+*       </h2>
+*   </div>
+*</div>
+*
+*/
+
+const parent1 = React.createElement(
+    "div",
+    { id: "parent1" },
+    [React.createElement(
+        "div",
+        { id: "child1" },
+        [
+            React.createElement("h1", {}, "I'm an h1 Tag"),
+            React.createElement("h2", {}, "I'm an h2 Tag"),
+        ]
+    ),
+    React.createElement(
+        "div",
+        { id: "child2" },
+        [
+            React.createElement("h1", {}, "I'm an h1 Tag"),
+            React.createElement("h2", {}, "I'm an h2 Tag"),
+        ]
+    )]
+)
+root.render(parent1)
