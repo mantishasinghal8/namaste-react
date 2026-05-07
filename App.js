3cf85e1f9a49ddd15a028fa9430d7b79f12931c1
mantishasinghal8 <mantishasinghal8@gmail.com>
2026-05-07 13:53:45 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,80 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// ~ React Element
+const jsxHeading = <h1 className="head">Namaste React using JSX</h1>;
 
-
-//~ Single Structure
-const heading = React.createElement(
-    "h1",//^ tag name
-    { id: "heading" }, //^ object that takes the attributes of the element  
-    "Hello World from React!"// ^ Data to be shown
+// ~ Title Component
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React Title Component using JSX
+  </h1>
 );
 
+// ~ elem Element
+const elem = <span>React Eleemnt in Span </span>;
+
+// ~ title Element
+const title = (
+  <h1 className="head" tabIndex="5">
+    {elem}
+    Namaste React title Element using JSX
+  </h1>
+);
+
+// ~ React Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+    {Title()}
+    <Title />
+    {title}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
 
-
-/* Normal Structure
-*<div id="parent">
-* <div id="child">
-*    <h1>
-*         I'm an h1 tag
-*      </h1>
-*   </div>
-*</div>
-*
-*/
-
-
-//~ Nested Structure
-// const parent = React.createElement(
-//     "div",
-//     { id: "parent" },
-//     React.createElement(
-//         "div",
-//         { id: "child" },
-//         React.createElement(
-//             "h1",
-//             {},
-//             "I'm an h1 Tag"
-//         )
-//     )
-// )
-
-// root.render(parent)
-
-/* Siblings Structure
-We will Convert create element to an array 
-*<div id="parent">
-*   <div id="child">
-*       <h1>
-*         I'm an h1 tag
-*       </h1>
-*       <h2>
-*         I'm an h2 tag
-*       </h2>
-*   </div>
-*</div>
-*
-*/
-
-const parent1 = React.createElement(
-    "div",
-    { id: "parent1" },
-    [React.createElement(
-        "div",
-        { id: "child1" },
-        [
-            React.createElement("h1", {}, "This is Namaste React🚀"),
-            React.createElement("h2", {}, "I'm an h2 Tag"),
-        ]
-    ),
-    React.createElement(
-        "div",
-        { id: "child2" },
-        [
-            React.createElement("h1", {}, "I'm an h1 Tag"),
-            React.createElement("h2", {}, "I'm an h2 Tag"),
-        ]
-    )]
-)
-root.render(parent1)
+root.render(<HeadingComponent />);
